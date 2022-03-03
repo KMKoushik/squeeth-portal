bb7b6bece5fde9e3f3f47cbddad0430d554e0d7b
Koushik KM <koushikmohan1996@gmail.com>
2022-03-03 21:50:04 +1100

    Add allowed URLS regex

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,16 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 const BLOCKED_COUNTRIES = ['US', 'BY', 'CU', 'IR', 'IQ', 'CI', 'LR', 'KP', 'SD', 'SY', 'ZW']
-const ALLOWED_URLS = ['images', 'fonts', 'favicon.ico']
+const ALLOWED_URLS = RegExp('(/images|/favicon.ico|/font)')
 
 export function middleware(req: NextRequest) {
-  const country = req?.geo?.country
+  const country = req?.geo?.country || 'US'
   const response = NextResponse.next()
   console.log(req.url)
-  const path = req.url.split('/')[1]
-  console.log(path, ALLOWED_URLS.includes(path))
+  const path = req.url
 
-  if (!ALLOWED_URLS.includes(path) && country && BLOCKED_COUNTRIES.includes(country)) {
+  if (country && BLOCKED_COUNTRIES.includes(country) && !ALLOWED_URLS.test(path)) {
     return NextResponse.rewrite('/blocked')
   }
 
