b0779b152b46a68c1be862449df3838738680065
Koushik KM <koushikmohan1996@gmail.com>
2022-03-03 21:35:03 +1100

    Add logs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -6,7 +6,9 @@ const ALLOWED_URLS = ['images', 'fonts', 'favicon.ico']
 export function middleware(req: NextRequest) {
   const country = req?.geo?.country
   const response = NextResponse.next()
+  console.log(req.url)
   const path = req.url.split('/')[1]
+  console.log(path, ALLOWED_URLS.includes(path))
 
   if (!ALLOWED_URLS.includes(path) && country && BLOCKED_COUNTRIES.includes(country)) {
     return NextResponse.rewrite('/blocked')
