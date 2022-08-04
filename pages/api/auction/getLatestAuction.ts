eca7931934450edba315a5896b480d9f82f6ea5e
Demola <66267188+DemolaJames@users.noreply.github.com>
2022-08-04 18:47:57 +0100

    Fetches current auction (#23)
    * Auction v2
    
    * Add more auction changes
    
    * Add error checks
    
    * Add Auction Details & Utilities to Auction Page (#18)
    
    * auction details
    
    * utils and formatting
    
    Co-authored-by: Darya Kaviani <daryakaviani@daryas-mbp.lan>
    
    * Auto change auction status
    
    * Fix conflicts
    
    * Add manual selection
    
    * Update auction url
    
    * Add address check
    
    * ui: copy changes (#20)
    
    * ui: copy changes
    
    * fix: build issue
    
    * Add Balance Field and Set to Max On-Click (#19)
    
    * balance field
    
    * change weth case to osqth amt
    
    * remove excess quantity
    
    * add back price changes
    
    Co-authored-by: Darya Kaviani <daryakaviani@daryas-mbp.lan>
    Co-authored-by: Darya Kaviani <daryakaviani@Daryas-MacBook-Pro.local>
    
    * Add admin dashboard changes
    
    * Add toast message and min size
    
    * Remove estimate clearing price
    
    * Mainnet firebase
    
    * add CHAIN-ID
    
    * Fix always showing selling
    
    * Fetches current auction
    
    * fix linting issue
    
    * auction live check fix
    
    * prettier fix
    
    * update response
    
    * remove unused file
    
    Co-authored-by: KMKoushik <koushikmohan1996@gmail.com>
    Co-authored-by: Darya Kaviani <daryakavi@gmail.com>
    Co-authored-by: Darya Kaviani <daryakaviani@daryas-mbp.lan>
    Co-authored-by: Alexis Gauba <alexisgauba@gmail.com>
    Co-authored-by: Darya Kaviani <daryakaviani@Daryas-MacBook-Pro.local>
    Co-authored-by: Andrew Leone <5490766+aleone@users.noreply.github.com>

---
+++ pages/api/auction/getLatestAuction.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getAuction } from '../../../server/utils/firebase-admin'
+import { Auction, AuctionStatus } from '../../../types'
+import { getAuctionStatus } from '../../../utils/auction'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') return res.status(400).json({ message: 'Only get is allowed' })
+
+  const auction = (await getAuction()).data() as Auction
+
+  if (!auction) res.status(200).json({ isLive: false, message: 'No auction created' })
+
+  const status = getAuctionStatus(auction)
+  const isLive = status === AuctionStatus.LIVE ? true : false
+
+  res
+    .status(200)
+    .json({ auction: auction, isLive: isLive, status: AuctionStatus[status], message: 'Retrieve successful' })
+}
