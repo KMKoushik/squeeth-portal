56028737dcf586782fe4d2dd3ae015643c9ab617
Demola <66267188+DemolaJames@users.noreply.github.com>
2022-08-05 02:14:51 +0100

    fetch user bids endpoint added

---
+++ pages/api/auction/getUserBids.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getUserBids } from '../../../server/utils/firebase-admin'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') return res.status(400).json({ message: 'Only get is allowed' })
+  const { user } = req.body
+  let bids: any
+  let message = 'Retrieve successful'
+  try {
+    bids = await getUserBids(user)
+
+    if (bids.length < 1) message = 'No Bids made for current auction'
+  } catch (e) {
+    return res.status(400).json({ message: 'Could not get user bids' })
+  }
+  res.status(200).json({ bids, message })
+}
