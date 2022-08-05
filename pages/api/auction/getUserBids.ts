a5637c0b2f7f2e548bde3ea05ab80d98f01fa3fa
Demola <66267188+DemolaJames@users.noreply.github.com>
2022-08-05 10:17:53 +0100

    update query param

--- pages/api/auction/getUserBids.ts
+++ pages/api/auction/getUserBids.ts
@@ -3,11 +3,13 @@ import { getUserBids } from '../../../server/utils/firebase-admin'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') return res.status(400).json({ message: 'Only get is allowed' })
-  const { user } = req.body
+  const { user } = req.query.user ? req.query : { user: '' }
+
+  if (user == '') return res.status(400).json({ message: 'No user address supplied' })
   let bids: any
   let message = 'Retrieve successful'
   try {
-    bids = await getUserBids(user)
+    bids = await getUserBids(user as string)
 
     if (bids.length < 1) message = 'No Bids made for current auction'
   } catch (e) {
