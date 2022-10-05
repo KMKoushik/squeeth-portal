3574320c06caacd09ca629ffd448d40639ec955c
dee-o <75335050+dee-o@users.noreply.github.com>
2022-10-05 20:53:01 +0100

    fix cross site manifest access

--- pages/auction-admin.tsx
+++ pages/auction-admin.tsx
@@ -50,7 +50,7 @@ const AuctionAdmin: NextPage = () => {
     <div>
       <Head>
         <title>Squeeth Portal - Auction admin</title>
-        <link rel="manifest" href="/manifest.json"></link>
+        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials"></link>
       </Head>
       <Nav />
       <Grid container spacing={5}>
