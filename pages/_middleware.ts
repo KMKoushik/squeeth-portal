29dc0f1b34c97309fa172d0799fae60f6091e87c
alpinechicken <joecmail@gmail.com>
2023-10-20 13:28:02 -0700

    ip whitelist

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,19 +1,23 @@
+import { request } from 'http'
 import { NextRequest, NextResponse } from 'next/server'
 
 const BLOCKED_COUNTRIES = ['US', 'BY', 'CU', 'IR', 'IQ', 'CI', 'LR', 'KP', 'SD', 'SY', 'ZW', 'CA']
+const ALLOWED_IPS = ['54.217.161.151']
 const ALLOWED_URLS = RegExp(
-  '(/images|/favicon.ico|/font|/api/auction/getLatestAuction|/api/auction/getUserBids|/api/auction/createOrEditBid|/api/auction/deleteBid|/api/auction/getAuctionById|/api/auction/getLastHedge)',
+  '(/images|/favicon.ico|/font|/api/auction/getLatestAuction|/api/auction/getUserBids|/api/auction/getAuctionById|/api/auction/getLastHedge)',
 )
 
 export function middleware(req: NextRequest) {
+  const userIp = req?.ip
   const country = req?.geo?.country
   const response = NextResponse.next()
   const path = req.url
 
-  if (country && BLOCKED_COUNTRIES.includes(country) && !ALLOWED_URLS.test(path)) {
+  if (country && userIp && BLOCKED_COUNTRIES.includes(country) && !ALLOWED_URLS.test(path) && !ALLOWED_IPS.includes(userIp)) {
     return NextResponse.rewrite('/blocked')
   }
-
+  // console.log(req.ip?.toString());
+  console.log('here with country ' + response.status)
   console.log(req.headers.get('sec-fetch-site') === 'cross-site')
   if (req.headers.get('sec-fetch-site') === 'cross-site') {
     req.nextUrl.searchParams.set('g-safe', 'true')
