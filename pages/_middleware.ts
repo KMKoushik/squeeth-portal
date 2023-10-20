81d31b42574da3b3aad09e87ddfc6f2a20160cdc
alpinechicken <joecmail@gmail.com>
2023-10-20 11:56:08 -0700

    fix api problem

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 const BLOCKED_COUNTRIES = ['US', 'BY', 'CU', 'IR', 'IQ', 'CI', 'LR', 'KP', 'SD', 'SY', 'ZW', 'CA']
 const ALLOWED_URLS = RegExp(
-  '(/images|/favicon.ico|/font|/api/auction/getLatestAuction|/api/auction/getAuctionById|/api/auction/getLastHedge)',
+  '(/images|/favicon.ico|/font|/api/auction/getLatestAuction|/api/auction/getUserBids|/api/auction/createOrEditBid|/api/auction/deleteBid|/api/auction/getAuctionById|/api/auction/getLastHedge)',
 )
 
 export function middleware(req: NextRequest) {
