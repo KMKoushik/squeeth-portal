3dfbfd3944cf81ff3a8fa5a5bdef4477951612c8
nikhil arora <narora200@gmail.com>
2023-12-05 12:40:33 +0530

    fix: wrap redis operations in try catch

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -22,9 +22,14 @@ export async function middleware(request: NextRequest) {
   const isIPWhitelisted = ip && allowedIPs.includes(ip)
 
   if (ip && !isIPWhitelisted) {
-    const redisData = await redis.get(ip)
-    const isIPBlocked = !!redisData
+    let redisData
+    try {
+      redisData = await redis.get(ip)
+    } catch (error) {
+      console.error('Failed to get data from Redis:', error)
+    }
 
+    const isIPBlocked = !!redisData
     console.log('ip', ip, isIPBlocked, url.protocol, url.host, '/blocked')
     if (isIPBlocked && url.pathname !== '/blocked') {
       return NextResponse.redirect(`${url.protocol}//${url.host}/blocked`)
@@ -32,7 +37,12 @@ export async function middleware(request: NextRequest) {
 
     const isFromVpn = await isVPN(ip)
     if (isFromVpn && url.pathname !== '/blocked') {
-      await redis.set(ip, 1)
+      try {
+        await redis.set(ip, 1)
+      } catch (error) {
+        console.error('Failed to set data in Redis:', error)
+      }
+
       console.log('vpnip', ip, isFromVpn, '/blocked')
       return NextResponse.redirect(`${url.protocol}//${url.host}/blocked`)
     }
