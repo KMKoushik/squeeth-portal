13368a2ccb80a3420cab20de674a05ad19e941f4
nikhil arora <narora200@gmail.com>
2023-12-18 20:12:28 +0530

    restrict all api requests irrespective of where they are coming from

--- server/middlewares/restrict-access.ts
+++ server/middlewares/restrict-access.ts
@@ -5,26 +5,11 @@ import { isIPBlockedInRedis, isVPN } from '../../utils/vpn'
 import { redis } from '../../utils/redisClient'
 import { BLOCKED_IP_VALUE } from '../../constants/restrictions'
 
-const ALLOWED_HOSTS = ['squeethportal.xyz', 'auction.opyn.co']
-const ALLOWED_HOST_PATTERN = /^squeeth-portal-[a-zA-Z0-9-]+-opynfinance\.vercel\.app$/
-
 export const restrictAccessMiddleware: Middleware = async (request, response, next) => {
   const ip = requestIp.getClientIp(request)
 
   const allowedIPs = (process.env.WHITELISTED_IPS || '').split(',')
   const isIPWhitelisted = ip && allowedIPs.includes(ip)
-
-  const origin = request.headers['origin']
-  console.log({ request: JSON.stringify(request) })
-
-  // check if api request is from the squeethportal site
-  // if yes, we allow the request without any restrictions
-  const isRequestFromAllowedOrigin = origin && (ALLOWED_HOSTS.includes(origin) || ALLOWED_HOST_PATTERN.test(origin))
-  console.log({ isRequestFromAllowedOrigin, origin })
-  if (isRequestFromAllowedOrigin) {
-    await next()
-    return
-  }
 
   if (ip && !isIPWhitelisted) {
     const currentTime = Date.now()
